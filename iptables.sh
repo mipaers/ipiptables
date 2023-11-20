277864364d994f2f1eb07cd2465d375c9d2d3729
mipaers <128981856+mipaers@users.noreply.github.com>
2023-11-21 07:26:12 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-echo -e "\e[31m此脚本的作用是实现端口转发后重启失效的问题。\e[0m"
-echo -e "\e[32m请输入需要转发的端口：\e[32m"
+if [[ $EUID -ne 0 ]]; then
+    echo "请使用root权限运行此脚本。"
+    exit 1
+fi
+echo "此脚本的作用是实现端口转发后重启失效的问题。"
+echo "请输入需要转发的端口："
 read original_port
-echo "\e[32m请输入转发到哪个端口：\e[0m"
+echo "请输入转发到哪个端口："
 read replacement_port
 iptables_command="iptables -t nat -A PREROUTING -p tcp --dport $original_port -j REDIRECT --to-port $replacement_port"
 touch /root/iptables_rules
